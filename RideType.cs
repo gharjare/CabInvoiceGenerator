7ce7e8ba9bccdca38fad27044fe40c56364651e2
gharjare <gharjarechetan@gmail.com>
2023-01-11 14:00:57 +0530

    [chetan gharjare]:here we store types of ride normal and primium

---
+++ RideType.cs
@@ -0,0 +1,15 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator1
+{
+    public enum RideType
+    {
+        PREMIUM,
+        NORMAL
+    }
+}
