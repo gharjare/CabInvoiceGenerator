4a0d1b5b3997e0e150863265b0dab657f4f95ac6
gharjare <gharjarechetan@gmail.com>
2023-01-11 14:02:52 +0530

    [chetan gharjare]:here we create custom exception

---
+++ CabInvoiceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator1
+{
+    public class CabInvoiceException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            NULL_RIDES,
+            INVALID_USER_ID
+        }
+        ExceptionType Type;
+        public CabInvoiceException(ExceptionType type,string message) :base(message)
+        {
+            Type = type;
+        }
+    }
+}
