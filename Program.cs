533da2d3815f77f631c6f43b85b33f3bc4bca86d
gharjare <gharjarechetan@gmail.com>
2023-01-11 13:56:41 +0530

    [chetan gharjare]: write wlcm msg

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CabInvoiceGenerator1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+        }
+    }
+}
